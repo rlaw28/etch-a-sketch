e549faad1a59af772021b06cb9b6ae0766a0966d
rlaw28 <rashaunlawson28@gmail.com>
2023-01-04 22:14:35 -0500

    Add wrapper to each iteration of the grid and remove when a new one starts so they don't stack on each other.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
 const container = document.getElementById('container');
 const resetButton = document.getElementById('reset');
+const newGrid = document.getElementById('new-grid');
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+
     for (let i = 0; i < amtOfGrids; i++){
         const row = document.createElement('div')
         row.classList.add('grid-row')
+
         for (let j = 0; j < amtOfGrids; j++){
-            const widthAndHeight = 850 / sizeOfGrid
+            const widthAndHeight = 850 / amtOfGrids
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
@@ -16,22 +21,30 @@ const createGrid = (amtOfGrids) => {
             gridBox.addEventListener('mouseenter', () => {
             gridBox.style.backgroundColor = 'black'
             })
-            row.appendChild(gridBox)
+            // row.appendChild(gridBox)
             resetButton.addEventListener('click', () => {
-            gridBox.style.backgroundColor = 'white'
-})
+                gridBox.style.backgroundColor = 'white'
+            })
+              row.appendChild(gridBox)
         }
-        container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    container.appendChild(wrapper)
 }
 
-let sizeOfGrid = 16;
-createGrid(sizeOfGrid);
+createGrid(16)
 
-// resetButton.addEventListener('click', () => {
-//     gridBox.style.backgroundColor = 'white'
-// })
 
+newGrid.addEventListener('click', () => {
+    let userSize = Number(prompt('Enter the number you want for the size of your new grid.'))
+
+    while (userSize > 100) {
+        userSize = Number(prompt('Pick a number smaller than 100.'))
+    }
+    const wrapper = document.querySelector('.wrapper')
+    wrapper.remove()
+    createGrid(userSize)
+})
 
 
 
